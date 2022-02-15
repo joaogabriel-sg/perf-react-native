bd207b5d481a93911080f7ae98fb51ebe41ba5d7
João Gabriel <main.joaogabrielsg@gmail.com>
2022-02-15 11:33:17 -0300

    feat: add app

---
+++ src/screens/Home.tsx
@@ -0,0 +1,61 @@
+import React, { useState } from "react";
+import {
+  Button,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+} from "react-native";
+
+import { FriendList } from "../components/FriendList";
+
+export function Home() {
+  const [name, setName] = useState("");
+  const [friends, setFriends] = useState([]);
+
+  async function handleSearch() {
+    const response = await fetch(`http://192.168.0.112:3333/friends?q=${name}`);
+    const data = await response.json();
+    setFriends(data);
+  }
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Amigos</Text>
+
+      <TextInput
+        placeholder="Nome do amigo"
+        value={name}
+        onChangeText={setName}
+        style={styles.input}
+      />
+
+      <Button title="Buscar" onPress={handleSearch} />
+
+      <ScrollView style={styles.list}>
+        <FriendList data={friends} />
+      </ScrollView>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 25,
+    marginTop: 100,
+  },
+  title: {
+    fontSize: 18,
+    fontWeight: "bold",
+  },
+  input: {
+    borderWidth: 1,
+    padding: 7,
+    marginVertical: 10,
+  },
+  list: {
+    marginTop: 20,
+  },
+});
